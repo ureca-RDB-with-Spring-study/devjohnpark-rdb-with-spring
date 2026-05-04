2921833663bf0502860cb8c0f355730965f0d5e3
devjohnpark <devjohnpark@gmail.com>
2026-05-04 23:00:33 +0900

    [Fix] use test profile in SmartclearanceApplicationTests to avoid MySQL connection in CI

--- src/test/java/com/smartclearance/SmartclearanceApplicationTests.java
+++ src/test/java/com/smartclearance/SmartclearanceApplicationTests.java
@@ -2,8 +2,10 @@ package com.smartclearance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartclearanceApplicationTests {
 
 	@Test
